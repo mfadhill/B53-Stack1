0efe499d14cf655b911be396c96e031e5c793703
mfdhl <mfadhill554@gmail.com>
2024-02-07 11:26:34 +0700

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-console.log("Hello world");
+function getData(){
+    let name = document.getElementById("name").value //let name = **
+    let email = document.getElementById("email").value
+    let phone = document.getElementById("phone").value
+    let subject = document.getElementById("subject").value
+    
+    if(name == ""){
+        return alert("please type your name*")
+    } else if (email ==""){
+        return alert("please type your name*")
+    }
+
+    const emailDestination = "hi.dandi9(@gmail.com"
+
+    console.log(name)
+    console.log(email)
+    console.log(phone)
+    console.log(subject)
+}
